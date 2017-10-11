fe03b155ba67577b049f689b48344483ffec418a
yangmingsheng <yms2007@126.com>
2017-10-11 09:37:45 +0800

    初始化

---
+++ helloworld/src/main/java/com/inspur/demo/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.inspur.demo;
+/**
+ * Created by yangmingsheng on 2017/10/11.
+ */
+public class HelloWorld {
+    public static void main(String[]args){
+        System.out.println("helloworld");
+    }
+}
